857970cec6f0e74d0774653db91e6db8745b7927
itouhiro <itouhiro@local>
2020-03-08 21:40:18 +0900

    2020-03-08 adjust to M+Fonts project

---
+++ mplus_outline_fonts/mig.d/scripts/config.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+postscript = "circle-mplus"
+family = "Circle M+"
+author = "M+ FONTS PROJECT, itouhiro"
+os2_vendor = "Mig "
+url = "http://mix-mplus-ipa.osdn.jp/"
+year = "2020"
+version = "1.063a"
+
+license = {
+  'Japanese': '''\
+このフォントはフリー（自由な）ソフトウエアです。\
+あらゆる改変の有無に関わらず、また商業的な利用であっても、\
+自由にご利用、複製、再配布することができますが、\
+全て無保証とさせていただきます。\
+''',
+  'English (US)': '''\
+This font is free software. \
+Unlimited permission is granted to use, copy, and distribute it, \
+with or without modification, either commercially or noncommercially. \
+THIS FONT IS PROVIDED "AS IS" WITHOUT WARRANTY.\
+''',
+}
